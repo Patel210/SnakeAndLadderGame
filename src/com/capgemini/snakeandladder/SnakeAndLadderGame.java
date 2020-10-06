8e2793c1acaf80ef93fc64ac35d267e3eed4f527
Patel210 <pranavpatel2101997@gmail.com>
2020-10-06 19:40:02 +0530

    Modified: Initialized player position

--- src/com/capgemini/snakeandladder/SnakeAndLadderGame.java
+++ src/com/capgemini/snakeandladder/SnakeAndLadderGame.java
@@ -1,7 +1,9 @@
 package com.capgemini.snakeandladder;
 
 public class SnakeAndLadderGame {
+	private static int position = 0;
+
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Player at Start Position: " + position);
 	}
 }
