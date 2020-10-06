8042737560790acacca2a7793395f4d388a4209b
Patel210 <pranavpatel2101997@gmail.com>
2020-10-06 20:31:25 +0530

    Modified: Complete play for Two Players

--- src/com/capgemini/snakeandladder/SnakeAndLadderGame.java
+++ src/com/capgemini/snakeandladder/SnakeAndLadderGame.java
@@ -1,39 +1,89 @@
 package com.capgemini.snakeandladder;
 
 public class SnakeAndLadderGame {
-	private static int position = 0;
+	private static int positionPlayer1 = 0;
+	private static int positionPlayer2 = 0;
 	private static final int LADDER = 1;
 	private static final int SNAKE = 2;
-	private static int noOfTimesDieRolled = 0;
-
+	
 	public static void main(String[] args) {
-		System.out.println("Player at Start Position: " + position);
+		boolean player1Plays = false;
+		int toss = (int) (Math.random() * 10) % 2;
 
-		while (position != 100) {
-			int numberOnDie = (int) ((Math.random()) * 6) + 1; // Die Roll
+		if (toss == 1) {
+			player1Plays = true;
+		}
 
-			int checkPlay = (int) Math.floor(Math.random() * 10) % 3; // Check Play Options
-			if (checkPlay == LADDER) {
-				System.out.println("Horray! You got a ladder!");
-				position += numberOnDie;
+		while (positionPlayer1 != 100 && positionPlayer2 != 100) {
+			if (player1Plays) {
+				int numberOnDie = (int) ((Math.random()) * 6) + 1; // Die Roll
+				int checkPlay = (int) Math.floor(Math.random() * 10) % 3; // Check Play Options
 
-				if (position > 100) { // To restrict player from position > 100
-					position -= numberOnDie;
+				switch (checkPlay) {
+				case LADDER:
+					System.out.println("Horray Player1 ! You got a ladder!");
+					positionPlayer1 += numberOnDie;
+
+					if (positionPlayer1 > 100) { // To restrict player from position > 100
+						positionPlayer1 -= numberOnDie;
+					}
+					break;
+
+				case SNAKE:
+					System.out.println("Bummer Player1! You got a snake!");
+					positionPlayer1 -= numberOnDie;
+
+					if (positionPlayer1 <= 0) {
+						positionPlayer1 = 0; // To restrict player from position > 100
+					}
+					player1Plays = false;
+					break;
+					
+				default:
+					System.out.println("Bummer Player1! You got no play!");
+					player1Plays = false;
 				}
-
-			} else if (checkPlay == SNAKE) {
-				System.out.println("Bummer! You got a snake!");
-				position -= numberOnDie;
-
-				if (position <= 0) {
-					position = 0; // To restrict player from position > 100
-				}
+				System.out.println("Player1 position: "+positionPlayer1);
 			}
 
-			noOfTimesDieRolled++;
+			else {
+				int numberOnDie = (int) ((Math.random()) * 6) + 1; // Die Roll
+				int checkPlay = (int) Math.floor(Math.random() * 10) % 3; // Check Play Options
 
-			System.out.println("Position after " + noOfTimesDieRolled + " roll: " + position);
+				switch (checkPlay) {
+				case LADDER:
+					System.out.println("Horray Player2! You got a ladder!");
+					positionPlayer2 += numberOnDie;
+
+					if (positionPlayer2 > 100) { // To restrict player from position > 100
+						positionPlayer2 -= numberOnDie;
+					}
+					break;
+
+				case SNAKE:
+					System.out.println("Bummer Player2! You got a snake!");
+					positionPlayer2 -= numberOnDie;
+
+					if (positionPlayer2 <= 0) {
+						positionPlayer2 = 0; // To restrict player from position > 100
+					}
+					player1Plays = true;
+					break;
+
+				default:
+					System.out.println("Bummer Player2! You got no play!");
+					player1Plays = true;
+				}
+				System.out.println("Player2 position: "+positionPlayer2);
+			}
 		}
-		System.out.println("Number of Times die rolled to win the game: " + noOfTimesDieRolled);
+		
+		if (positionPlayer1 == 100) {
+			System.out.println("Player 1 won the game");
+		}
+		
+		else {
+			System.out.println("Player 2 won the game");
+		}
 	}
 }
