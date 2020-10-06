31154015f68bbf9a5fab1de76de14904f44ff009
Patel210 <pranavpatel2101997@gmail.com>
2020-10-06 19:43:02 +0530

    Modified: Die Rolled for the move

--- src/com/capgemini/snakeandladder/SnakeAndLadderGame.java
+++ src/com/capgemini/snakeandladder/SnakeAndLadderGame.java
@@ -5,5 +5,8 @@ public class SnakeAndLadderGame {
 
 	public static void main(String[] args) {
 		System.out.println("Player at Start Position: " + position);
+		
+		int numberOnDie = (int) ((Math.random()) * 6) + 1;
+		System.out.println("Player rolled: " + numberOnDie);
 	}
 }
